91a80ed4cb2f8a4f49e6154d9a3d302a541cbaec
unknown <ssilance@unb.ca>
2020-10-24 21:24:57 -0300

    Changed platform images

--- js/PlayState/states/preload.js
+++ js/PlayState/states/preload.js
@@ -3,12 +3,12 @@ export function preload() {
 
     // IMAGES
     this.game.load.image('background', 'images/background.png');
-    this.game.load.image('ground', 'images/ground.png');
-    this.game.load.image('grass:8x1', 'images/grass_8x1.png');
-    this.game.load.image('grass:6x1', 'images/grass_6x1.png');
-    this.game.load.image('grass:4x1', 'images/grass_4x1.png');
-    this.game.load.image('grass:2x1', 'images/grass_2x1.png');
-    this.game.load.image('grass:1x1', 'images/grass_1x1.png');
+    this.game.load.image('ground', 'images/blue_ground.png');
+    this.game.load.image('grass:8x1', 'images/blue_grass_8x1.png');
+    this.game.load.image('grass:6x1', 'images/blue_grass_6x1.png');
+    this.game.load.image('grass:4x1', 'images/blue_grass_4x1.png');
+    this.game.load.image('grass:2x1', 'images/blue_grass_2x1.png');
+    this.game.load.image('grass:1x1', 'images/blue_grass_1x1.png');
     this.game.load.image('hero', 'images/hero_stopped.png');
     this.game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22);
 
