87d2d1ced0cdaa91bb40124e86c5dfd2508e0414
unknown <ssilance@unb.ca>
2020-10-25 01:18:40 -0300

    Added start button

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@ import PlayState from "./PlayState/PlayState.js";
 
 const playstate = new PlayState();
 
-window.onload = function () {
+function launchGame() {
+    document.getElementById('canvasPlaceholder').style.display = 'none'; 
     let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
     game.state.add('play', playstate);
     game.state.start('play');
-};
+}
+
+document.getElementById('startButton').addEventListener('click', launchGame);
