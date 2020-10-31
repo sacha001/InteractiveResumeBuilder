8cb42d7b7cc92242c7dc561f670f3450300e7366
Sacha Silance <ssilance@unb.ca>
2020-10-31 16:05:26 -0300

    WIP - Adding ability to customize game background

--- public/game/js/launchGame.js
+++ public/game/js/launchGame.js
@@ -3,19 +3,9 @@ import PlayState from "./PlayState/PlayState.js";
 const playstate = new PlayState();
 
 export default function launchGame() {
-    // let canvas = document.getElementById('textCanvas');
-    // let ctx = canvas.getContext('2d');
-    // let img = document.getElementById('image');
-
- 
-    // ctx.font = '30px VT323';
-    // ctx.fillText("Almost before we knew it, we had left the ground", 0, 90);
+    
     
     let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
     game.state.add('play', playstate);
     game.state.start('play');
-    //img.src = ctx.canvas.toDataURL();
-    
-    
-
 }
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,21 @@
 import launchGame from "../game/js/launchGame.js";
 
 document.getElementById('startButton').addEventListener('click', () => {
-    document.getElementById('canvasPlaceholder').style.display = 'none'; 
-    launchGame();
+    //document.getElementById('canvasPlaceholder').style.display = 'none'; 
+    // launchGame();
+    let canvas = document.getElementById('textCanvas');
+    let ctx = canvas.getContext('2d');
+    ctx.textAlign = 'center';
+    ctx.font = '100px VT323';
+
+    let text = "SACHA SILANCE";
+
+    let img = document.getElementById('image');
+    let image = new Image();
+    image.src = "images/background.png";
+    image.onload = function(){
+        ctx.drawImage(image, 0, 0);
+        ctx.fillText(text, canvas.width/2, 90);
+        img.src = ctx.canvas.toDataURL();
+    } 
 });
