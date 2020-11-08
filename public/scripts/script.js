3dee901a2f35787df7eb164d7673ec80c9ffe438
Sacha Silance <ssilance@unb.ca>
2020-11-08 00:14:53 -0400

    Make genBackground a more general function

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,48 +2,27 @@ import launchGame from "../game/js/launchGame.js";
 
 document.getElementById('startButton').addEventListener('click', () => {
     document.getElementById('canvasPlaceholder').style.display = 'none';
-    genLvl1Backround();
-    genLvl2Backround();
+    genBackground(0, 'SACHA SILANCE', '50px "Press Start 2P"', '#000000', 960/2, 90);
+    genBackground(1, 'UNB', '25px "Press Start 2P"', '#FFFFFF', 960/2, 227);
 });
 
-function genLvl1Backround() {
+function genBackground(lvlNum, text, font, color, x, y) {
   let image = new Image();
-  image.src = "images/background-level-0.png";
+  image.src = "images/background-level-" + lvlNum + ".png";
   image.onload = function() {
     let canvas = document.createElement('canvas');
     canvas.width = 960;
     canvas.height = 600;
     let ctx = canvas.getContext('2d');
     ctx.textAlign = 'center';
-    ctx.font = '50px "Press Start 2P"';
+    ctx.fillStyle= color;
+    ctx.font = font;
 
-    let text = "SACHA SILANCE";
     ctx.drawImage(image, 0, 0);
-    ctx.fillText(text, canvas.width/2, 90);
+    ctx.fillText(text, x, y);
     let base64img = ctx.canvas.toDataURL();
-    let body = {base64img: base64img, filename: "background-level-0.png"};
-    postData('upload', body).then(launchGame());
-  }
-}
-
-function genLvl2Backround() {
-  let image = new Image();
-  image.src = "images/background-level-1.png";
-  image.onload = function() {
-    let canvas = document.createElement('canvas');
-    canvas.width = 960;
-    canvas.height = 600;
-    let ctx = canvas.getContext('2d');
-    ctx.textAlign = 'center';
-    ctx.fillStyle= '#FFFFFF';
-    ctx.font = '20px "Press Start 2P"';
-
-    let text = "UNB";
-    ctx.drawImage(image, 0, 0);
-    ctx.fillText(text, canvas.width/2, 225);
-    let base64img = ctx.canvas.toDataURL();
-    let body = {base64img: base64img, filename: "background-level-1.png"};
-    postData('upload', body);
+    let body = {base64img: base64img, filename: "background-level-" + lvlNum + ".png"};
+    postData('upload', body).then(() => { if (lvlNum === 0) { launchGame()} });
   }
 }
 
@@ -62,5 +41,5 @@ async function postData(url = '', data = {}) {
     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify(data) // body data type must match "Content-Type" header
   });
-  return response.json(); // parses JSON response into native JavaScript objects
+  return response;
 }
